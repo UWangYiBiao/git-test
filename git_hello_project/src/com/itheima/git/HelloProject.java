0be28d3a404cdb6dbf1981fa8470fd50e9179d98
John <18766113017@163.com>
2020-07-01 17:38:17 +0800

    添加新工程

---
+++ git_hello_project/src/com/itheima/git/HelloProject.java
@@ -0,0 +1,22 @@
+package com.itheima.git;
+
+public class HelloProject {
+
+    public static void main(String[] args) {
+        System.out.println("hello,git!!!");
+        System.out.println("hello,git!!!");
+        System.out.println("hello,git!!!");
+        System.out.println("hello,git!!!");
+        System.out.println("hello,git!!!");
+        System.out.println("hello,git!!!");
+        System.out.println("hello,git!!!");
+        System.out.println("hello,git!!!");
+        System.out.println("hello,git!!!");
+        System.out.println("hello,git!!!");
+        System.out.println("hello,git!!!");
+        System.out.println("hello,git!!!");
+        System.out.println("hello,git!!!");
+        System.out.println("hello,git!!!");
+        System.out.println("hello,git!!!");
+    }
+}
